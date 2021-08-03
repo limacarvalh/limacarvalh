fcd25fbebf2ad9eeda55518a8ebc953db29045fb
marcos.carvalho1 <marcos.carvalho1@PLE111D28.alunos.sc.senac.br>
2021-08-03 19:24:57 -0300

    subindo projeto...

---
+++ src/util/GeradorUtil.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author silvio.junior
+ */
+public class GeradorUtil {
+
+    /**
+     * Retorna um valor String conforme a quantidade de número solcitado
+     *
+     * @param qtde
+     * @return
+     */
+    public String gerarNumero(int qtde) {
+        String senha = "";
+        int numero;
+        for (int i = 0; i < qtde; i++) {
+            numero = (int)(Math.random() * 10);
+            senha = senha + numero;
+        }
+        return senha;
+    }
+    
+    public String gerarCpf(){
+        return gerarNumero(3) + "." + gerarNumero(3) + "." + gerarNumero(3) + "-" 
+                + gerarNumero(2);
+    }
+
+    public double gerarSalario() {
+        int numero = (int) (Math.random() * 100000);
+        return numero;
+    }
+
+    public static void main(String[] args) {
+        GeradorUtil util = new GeradorUtil();
+        String cpf = util.gerarCpf();
+        System.out.println("CPF: " + cpf);
+    }
+}
